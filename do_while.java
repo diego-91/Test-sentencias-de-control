d690e0bf236a767c1765f4a44a95994174422b21
diego <diego_g_ramirez@hotmail.com>
2022-11-10 21:03:07 -0300

    Realicé los 5 ejercicios de condicionales

---
+++ do_while.java
@@ -0,0 +1,10 @@
+public class While {
+
+	public static void main(String[] args){
+		int numeroWhile = 1;
+		do{
+	    	System.out.println(numeroWhile);	   
+	    	numeroWhile = -0;
+	}   while(numeroWhile > 0);
+		}
+}
---
+++ if.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+			int numeroif = 4;
+
+			if (numeroif < 0) {
+				System.out.println("es negativo");
+			} else if (numeroif > 0) {
+				System.out.println("Es positivo");
+			} else {
+				System.out.println("Es 0");
+			}
+	}
+}
